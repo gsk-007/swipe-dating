fe69a38d5b25eff9cb250ab2fd78bfebdf6e9020
gsk-007 <gsingh16742@gmail.com>
2025-04-15 11:53:20 +0530

    feat: add routes files with boilerplate

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// routes
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import matchRoutes from "./routes/matchRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/matches", matchRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, console.log("Listening on port " + PORT));
