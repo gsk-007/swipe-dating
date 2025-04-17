107d8372a763ffd036ba83c6a730abca07061b94
gsk-007 <gsingh16742@gmail.com>
2025-04-17 10:14:10 +0530

    feat(api): add user profile update route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { protectRoute } from "../middlewares/auth.js";
+import { updateProfile } from "../controllers/userController.js";
 
 const router = express.Router();
+
+router.put("/update", protectRoute, updateProfile);
 
 export default router;
