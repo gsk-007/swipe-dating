79351fa22e066ce842c15ce2cb1754f5824c59b6
gsk-007 <gsingh16742@gmail.com>
2025-04-19 12:13:59 +0530

    feat(client): add routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
+import ChatPage from "./pages/ChatPage";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello World</h1>
-    </>
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
+      </Routes>
+    </div>
   );
 }
 
