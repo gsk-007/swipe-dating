f25788f1f8001cb105b771b99c96e770ab741ae3
gsk-007 <gsingh16742@gmail.com>
2025-04-19 14:37:23 +0530

    feat(client): add auth store using zustand, add login and signup functionality to app

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const SignUpForm = () => {
   const [name, setName] = useState("");
@@ -8,10 +9,16 @@ const SignUpForm = () => {
   const [age, setAge] = useState("");
   const [genderPreference, setGenderPreference] = useState("");
 
-  const loading = false;
+  const { signup, loading } = useAuthStore();
 
   return (
-    <form className="space-y-1">
+    <form
+      className="space-y-1"
+      onSubmit={(e) => {
+        e.preventDefault();
+        signup({ name, email, password, age, gender, genderPreference });
+      }}
+    >
       {/* NAME */}
       <div>
         <label
