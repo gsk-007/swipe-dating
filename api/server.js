ece58694b5bfe22d081074507f4a6df88f8974d1
gsk-007 <gsingh16742@gmail.com>
2025-04-24 08:02:48 +0530

    feat: production changes

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { createServer } from "http";
+import path from "path";
 
 // routes
 import authRoutes from "./routes/authRoutes.js";
@@ -15,9 +16,11 @@ import { initializeSocket } from "./socket/socket.server.js";
 dotenv.config();
 
 const app = express();
+const httpServer = createServer(app);
 const PORT = process.env.PORT || 5000;
 
-const httpServer = createServer(app);
+const __dirname = path.resolve();
+
 initializeSocket(httpServer);
 
 app.use(express.json());
@@ -33,6 +36,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/matches", matchRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 httpServer.listen(PORT, () => {
   console.log("Server started at port: " + PORT);
